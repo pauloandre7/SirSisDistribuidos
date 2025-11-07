04607cafe9e0c052006f36fdb2344ca8ba1c442f
Paulo André <andrepaulos.2001@gmail.com>
2025-11-07 07:08:22 -0300

    Initial point: implements sequencial solution without SIS model

---
+++ src/main/java/com/pauloandre7/App.java
@@ -0,0 +1,13 @@
+package com.pauloandre7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/pauloandre7/AppTest.java
@@ -0,0 +1,15 @@
+package com.pauloandre7;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.DisplayName;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("Teste Simples para verificar se a App funciona")
+    void shouldAnswerWithTrue() {
+        // Altere o teste para refletir a lógica da sua aplicação
+        assertTrue(true);
+    }
+}
